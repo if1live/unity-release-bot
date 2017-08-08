fca571301a4502052befff1923da2d57c826d4a5
if1live <libsora25@gmail.com>
2017-08-08 22:07:43 +0900

    testdata for unti 2017

--- fetcher_test.go
+++ fetcher_test.go
@@ -12,15 +12,36 @@ func TestExtractVersion(t *testing.T) {
 		filepath string
 		version  string
 	}{
+		// url
 		{
 			"http://netstorage.unity3d.com/unity/38b4efef76f0/UnityDownloadAssistant-5.5.0f3.exe",
 			"5.5.0f3",
 		},
-		{"UnityDownloadAssistant-5.5.0f3.exe", "5.5.0f3"},
-		{"Unity-12.34.56f78.exe", "12.34.56f78"},
-		{"UnityDaydreamDownloadAssistant-5.4.2f2-GVR13.exe", "5.4.2f2-GVR13"},
-		{"UnityStandardAssetsSetup-5.5.1f1.exe", "5.5.1f1"},
-		{"CacheServer-5.5.1f1.zip", "5.5.1f1"},
+		// 5.x
+		{
+			"UnityDownloadAssistant-5.5.0f3.exe",
+			"5.5.0f3",
+		},
+		// postfix
+		{
+			"UnityDaydreamDownloadAssistant-5.4.2f2-GVR13.exe",
+			"5.4.2f2-GVR13",
+		},
+		// different prefix
+		{
+			"UnityStandardAssetsSetup-5.5.1f1.exe",
+			"5.5.1f1",
+		},
+		// unity 2017
+		{
+			"UnityDownloadAssistant-2017.1.0p2.exe",
+			"2017.1.0p2",
+		},
+		// format extension
+		{
+			"Unity-12.34.56f78.exe",
+			"12.34.56f78",
+		},
 	}
 
 	for _, c := range cases {
@@ -32,13 +53,23 @@ func TestExtractVersion(t *testing.T) {
 }
 
 func TestGetLatestVersion(t *testing.T) {
-	src, _ := ioutil.ReadFile("testdata/download-stable-5.5.1f1.html")
-	f := &FakeHTTPFetcher{
-		idx:     0,
-		sources: []string{string(src)},
+	cases := []struct {
+		file    string
+		version string
+	}{
+		{"testdata/download-stable-5.5.1f1.html", "5.5.1f1"},
+		{"testdata/download-stable-2017.1.0f3.html", "2017.1.0f3"},
 	}
-	version := getLatestVersion(f)
-	assert.Equal(t, "5.5.1f1", version)
+
+	for _, c := range cases {
+		src, _ := ioutil.ReadFile(c.file)
+		f := &FakeHTTPFetcher{
+			idx:     0,
+			sources: []string{string(src)},
+		}
+		version := getLatestVersion(f)
+		assert.Equal(t, c.version, version)
+	}
 }
 
 func TestMakeStableReleaseNoteURL(t *testing.T) {
@@ -48,6 +79,8 @@ func TestMakeStableReleaseNoteURL(t *testing.T) {
 	}{
 		{"5.5.0f3", "https://unity3d.com/kr/unity/whats-new/unity-5.5.0"},
 		{"5.5.0", "https://unity3d.com/kr/unity/whats-new/unity-5.5.0"},
+		{"2017.1.0f3", "https://unity3d.com/kr/unity/whats-new/unity-2017.1.0"},
+		{"2017.1.0", "https://unity3d.com/kr/unity/whats-new/unity-2017.1.0"},
 		{"invalid", ""},
 	}
 	for _, c := range cases {
